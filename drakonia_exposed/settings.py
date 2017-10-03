6d911482a00a04790203a851027cf0428150955e
Aleister777 <aleisterwilliams@gmail.com>
2017-10-03 00:01:24 -0400

    added media folder functionality

--- drakonia_exposed/settings.py
+++ drakonia_exposed/settings.py
@@ -120,19 +120,31 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') #'static' also works here? 
 
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
 	os.path.join(PROJECT_ROOT, 'static'),
-]
+)
 
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media') #BASE_DIR switched out, now works
+MEDIA_URL  = '/media/'
 
 
+
+"""
+#these have to be added to enable static files
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL  = '/media/'
+"""
--- drakonia_exposed/urls.py
+++ drakonia_exposed/urls.py
@@ -18,16 +18,23 @@ from django.contrib import admin
 
 from . import views
 
+admin.autodiscover() #not sure what this is
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index')
 ]
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
 
-"""
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 #this will most likely be necessary for locally loading media
-if settings.DEBUG is True:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-"""
+#if settings.DEBUG is True:
+#	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
